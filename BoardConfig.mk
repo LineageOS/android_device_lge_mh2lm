a6f34f75dfaf44d5db4a9a17022ada3992242b20
Han Sol Jin <hansol@hansol.ca>
2024-06-20 10:30:12 -0700

    mh2lm: use Soong config to handle custom light include directory
    Change-Id: I828325be95e249fd7da407d7d27ab7c24209eb93

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,6 @@ DEVICE_PATH := device/lge/mh2lm
 # inherit from common tree
 -include device/lge/sm8150-common/BoardConfigCommon.mk
 
-# Platform
-TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
-
 # Display
 TARGET_SCREEN_DENSITY := 401
 
@@ -22,6 +19,11 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lineageos_mh2_defconfig
+
+# Platform
+SOONG_CONFIG_NAMESPACES += LGE_LIGHTS_HAL
+SOONG_CONFIG_LGE_LIGHTS_HAL := INCLUDE_DIR
+SOONG_CONFIG_LGE_LIGHTS_HAL_INCLUDE_DIR := $(DEVICE_PATH)/include
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop

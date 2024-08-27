23d8f8fe47554c856ba0b3e3c151d6aaf726d353
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    mh2lm: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/hw/fingerprint.msmnile.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libRbsFlow_shim.so" "${2}"
             ;;
         vendor/lib*/liblgdnnsnpe.so|vendor/lib*/liblgsnpeawb.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed libstdc++.so libstdc++_vendor.so "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -28,5 +38,6 @@ set -e
 export DEVICE=mh2lm
 export DEVICE_COMMON=sm8150-common
 export VENDOR=lge
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
